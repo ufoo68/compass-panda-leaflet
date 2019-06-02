1c77297aeb3538aaeb8e935567b1f98d0764b125
ufoo68 <mitarou.yuutarou@gmail.com>
2019-06-02 09:32:10 +0900

    leaflet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+import Leaflet from 'leaflet'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import SimpleExample from './components/simple'
+
+Leaflet.Icon.Default.imagePath =
+  '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SimpleExample />
     );
   }
 }
